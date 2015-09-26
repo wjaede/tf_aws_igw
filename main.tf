ffa136fa2eb130c09a787cfd15fcdb2fadd0c9cd
wolfgang.jaede <wolfgang.jaede@bauerxcel.de>
2015-09-26 19:12:00 +0200

    multi regeion update

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+/* Setup our aws provider */
+provider "aws" {
+  access_key  = "${var.access_key}"
+  secret_key  = "${var.secret_key}"
+  region      = "${var.region}"
+}
 /* Internet gateway for the public subnet */
 resource "aws_internet_gateway" "igw" {
   vpc_id = "${var.vpc_id}"
